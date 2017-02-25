5c29652a7a72c9e2f3b7327e6a36acdfd5cfc6f4
OcTaGa <fabien.cano@gmail.com>
2017-02-25 20:00:54 +0100

    Add files via upload

---
+++ dht11.ino
@@ -0,0 +1,13 @@
+#include <DHT.h>
+float t=0;
+void setup() {
+        Serial.begin(9600);        
+}
+
+void loop() {
+        DHT dht(2, DHT11);
+        dht.begin();
+        t = dht.readTemperature();
+        Serial.println("Temperature: ");
+        Serial.println(t);
+}
