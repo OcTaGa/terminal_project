b0d255f0eae6defff7bb8af8db8fefb68fdb3ce9
OcTaGa <fabien.cano@gmail.com>
2017-02-27 17:19:00 +0100

    Add files via upload

--- dht11.ino
+++ dht11.ino
@@ -1,13 +1,25 @@
 #include <DHT.h>
-float t=0;
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
+
+float temp=0;
+LiquidCrystal_I2C lcd(0x27,16,2);
+
 void setup() {
-        Serial.begin(9600);        
+        lcd.begin();
+        //Serial.begin(9600);        
 }
 
 void loop() {
         DHT dht(2, DHT11);
         dht.begin();
-        t = dht.readTemperature();
-        Serial.println("Temperature: ");
-        Serial.println(t);
+        temp = dht.readTemperature();
+        lcd.noCursor();
+        
+        lcd.print("temperature: ");
+        //Serial.println("Temperature: ");
+        //Serial.println(temp);
+        //lcd.setCursor(0, 1);
+        lcd.print(temp);
+        delay(2000);
 }
