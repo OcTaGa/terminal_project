7840b26a3e93617c4b88716072d71979b5101d87
OcTaGa <fabien.cano@gmail.com>
2017-03-23 08:40:55 +0100

    Update Hysteresis
    version original a update depuis le PC de chez moi

--- dht11.ino
+++ dht11.ino
@@ -7,8 +7,10 @@
 float t=0;
 int PWM_A = LOW; // moteur ventilateur
 int PWM_B = LOW; // moteur fenetre
+float temp_cons = 0;
+float hysteresis = 2;
 float temp_act = 0;
-
+  /* ================= DEFINITION DU CLAVIER ========================= */
 const byte ROWS = 4;
 const byte COLS = 3;
 char keys[ROWS][COLS]  = {
@@ -19,9 +21,11 @@ char keys[ROWS][COLS]  = {
 };
 byte rowPins[ROWS] = {6, 5, 4, 3};
 byte colPins[COLS] = {9, 8, 7};
-
+/* ========================= INITIALISATION DU LCD/KEYPAD ================== */ 
 LiquidCrystal_I2C lcd(0x27,20,4);
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
+/* ========================= DEFINITIONS DES PINs ========================= */
 
 void setup() {
       //PID(RES_M, Adj_res, Value_reach,  );
@@ -33,6 +37,9 @@ void setup() {
      //Serial.begin(9600); 
 
 }
+
+/* ============================================================================ */
+
 void loop() {
 
       DHT dht(2, DHT11);
@@ -40,42 +47,43 @@ void loop() {
       t = dht.readTemperature();
       float h = dht.readHumidity();
       char temp_min;
-      if (temp_min = NO_KEY) {
-          temp_min = keypad.getKey();
-          lcd.setCursor(1,3);
-          lcd.print(temp_min);
+  /* ==================== AFFICHAGE TEMPERATURE DE CONSIGNE ========================== */
+      if (temp_cons == NO_KEY ) {
+          temp_cons = keypad.getKey();
+          lcd.setCursor(3, 0);
+          lcd.print(temp_cons);
+       
       }
-      char key = keypad.getKey();
+     char key = keypad.getKey();
       if (key == '#') {
             lcd.setCursor(1, 3);
             lcd.clear();
       
       }
-
-      if (t < temp_min)  {
-          pinMode(3, HIGH);
+   /* ================= HYSTERESIS: THermostat temperature =========================== */
+      if (t <= temp_cons - hysteresis)  {
+          
+          digitalWrite(3, HIGH);
+          digitalWrite(PWM_A , LOW);
+          digitalWrite(PWM_B, LOW);
           lcd.setCursor(8, 3);
-          lcd.print("heat ON !!");
+          lcd.print("heat ON ,Fan OFF");
       }
-      else {
-          pinMode(3, LOW);
-          pinMode(PWM_A, LOW);
+      if (t => temp_cons - hysteresis ) [
+          digitalWrite(3, LOW);
+          digitalWrite(PWM_A, HIGH);
+          digitalWrite(PWM_B, HIGH);
           lcd.setCursor(8, 3);
-          lcd.print("heat OFF !!");
+          lcd.print("Heat OFF, Fan ON");
         
       }
-    /*  if (t > temp_max) {
-
-            pinMode(3, LOW);
-            lcd.setCursor(8, 3);
-            pinMode(PWM_A, HIGH);
-            pinMode(PWM_B, HIGH);
-      } */
+   /* ======================GESTION DE L'ECRAN ======================================= */
+   
       lcd.clear();
       lcd.print("Temp : ");
       lcd.print(t);
       lcd.setCursor(9, 0);
-      lcd.print(" C");
+      lcd.print(" °C");
       lcd.setCursor(0, 1);
       lcd.print("Humidity :  ");
       lcd.setCursor(11, 1); 
