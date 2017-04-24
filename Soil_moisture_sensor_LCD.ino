3ebebd03c0ff526ec25d6226a2ef445b9e156c94
NekoSquid <neuronette31@outlook.fr>
2017-04-24 14:40:04 +0200

    Add files via upload

---
+++ Soil_moisture_sensor_LCD.ino
@@ -0,0 +1,34 @@
+#include <LiquidCrystal.h> //include the LCD library
+LiquidCrystal lcd(12, 11, 7, 6, 5, 4);
+
+
+int sensorPin = A0;    // select the input pin for the potentiometer
+int sensorValue = 0;  // variable to store the value coming from the sensor
+ 
+void setup() {
+  lcd.begin(16, 2);
+  lcd.print("Humidite");
+  // declare the ledPin as an OUTPUT:
+   Serial.begin(9600);
+}
+ 
+void loop() {
+  // read the value from the sensor:
+  sensorValue = analogRead(sensorPin);
+  delay(1000);
+  Serial.print("sensor = " );
+  Serial.println(sensorValue);
+  int sensorValue = analogRead(sensorPin) ;
+  sensorValue = constrain(sensorValue, 485, 1023);
+  sensorValue = map(sensorValue, 485, 1023, 100, 0);
+//display final percentage
+  lcd.print(sensorValue);
+//print the percent symbol at the end
+  lcd.print("%");
+//wait 0.1 seconds
+  delay(75);
+//wipe the extra characters
+  lcd.print(" ");
+  delay (1);
+ 
+}
---
+++ Soil_moisture_sensor_LED.ino
@@ -0,0 +1,45 @@
+int output_value ;
+
+int led_pin =13;
+
+int sensor_pin =A0;
+
+void setup() {
+
+    Serial.begin(9600);
+
+    Serial.println("Reading From the Sensor ...");
+
+    delay(2000);
+
+    pinMode(led_pin, OUTPUT);
+
+    pinMode(sensor_pin, INPUT);
+
+}
+
+void loop() {
+  
+ output_value= analogRead(sensor_pin);
+
+ output_value = map(output_value,550,0,0,100);
+
+ Serial.print("Moisture : ");
+
+ Serial.print(output_value);
+
+ Serial.println("%");
+
+ delay(1000);
+
+ if(digitalRead(sensor_pin) == HIGH){
+
+ digitalWrite(led_pin, LOW);
+
+ } else {
+
+  digitalWrite(led_pin, HIGH);
+
+  delay(1000);
+ }
+}
